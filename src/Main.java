1a58267a6c29008ddd8687e9b93346a379d1a6f7
Taty ts <tatytsareva@mail.ru>
2024-09-19 17:39:42 +0300

    src/Main.java added

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 13676;
+        int RUB = 20;
+
+
+        int Bonus = (price / RUB);
+        System.out.println(Bonus);
+
+        String name = "Бонус";
+        System.out.println("Бонус" + (" ") + price / RUB);
+    }
+}
+
